3d685a3fef305ce2cca7413b470822196c99bb02
imtangx <117887725+imtangx@users.noreply.github.com>
2024-10-07 12:19:00 +0800

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 void swap(int a[],int i,int j)
 {
+	//update
 	int k=a[i];
 	a[i]=a[j];
 	a[j]=k;
@@ -10,15 +11,15 @@ int main()
 	int a[]={};
 	int length=sizeof(a)/sizeof(a[0]);
 	int i,j;
-	for(i=length-1;i>0;i--) //j+1µÄ´æÔÚ,i=length-1ÒÔ·ÀÔ½½ç
+	for(i=length-1;i>0;i--) //j+1çš„å­˜åœ¨,i=length-1ä»¥é˜²è¶Šç•Œ
 	{
 		for(j=0;j<i;j++) 
 		{
 			if(a[j+1]<a[j]) 
-			swap(a,j,j+1); //´óµÄÊýÍùÓÒ³Á 
+			swap(a,j,j+1); //å¤§çš„æ•°å¾€å³æ²‰ 
 		}
 	}	
-	//¼ìÑé 
+	//æ£€éªŒ 
 	for(i=0;i<length;i++)
 	printf("%-3d",a[i]);
 	return 0;
